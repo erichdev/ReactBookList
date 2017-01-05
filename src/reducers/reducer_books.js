1d094c22724a8052c5362c31d7203b753d645157
Erich Abellera <eabellera@athletics.ucla.edu>
2017-01-05 12:54:36 -0800

    Initial commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+   return [
+      { title: 'Harry Potter'},
+      { title: 'The Lord of the Rings'},
+      { title: 'Imagination'},
+      { title: 'Biography'}
+   ];
+}
